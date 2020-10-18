4abc3d02cb5049cde5c28d2683ffc3fbae859eac
matee98 <matzab37@gmail.com>
2020-10-18 12:39:31 +0200

    Sieve of Ertosthenes implementation

---
+++ sieveOfEratosthenes.py
@@ -0,0 +1,22 @@
+# sieve of Eratosthenes
+
+from math import sqrt
+
+
+def sieveOfEratosthenes(start, end):
+    res = []
+    it = start
+    while it <= end:
+        res.append(it)
+        it += 1
+    det = sqrt(end)
+    ind = 2
+    while ind <= det:
+        for i in res:
+            if (i % ind == 0) & (i > ind):
+                res.remove(i)
+        ind += 1
+    print(res)
+
+
+sieveOfEratosthenes(2, 100)
