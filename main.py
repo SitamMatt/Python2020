da1179232589157fecda2c586ef6c62a0a1ed37a
SitamMatt <mateuszszewc@op.pl>
2020-10-19 16:28:50 +0200

    wallis improvements

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def wallis(k):
-    result = 1
-    for n in range(1, k + 1):
-        p1 = (2 * n) / (2 * n - 1)  # 1st part
-        p2 = (2 * n) / (2 * n + 1)  # 2nd part
-        p = p1 * p2  # final iteration result
-        result *= p
-        # the final result should be multiplied by 2 because Wallis product gives approximation of pi/2
-    return result * 2
-
+from wallis import wallis
 
 print(wallis(10))
